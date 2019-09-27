99e1e86269a92e4113986a69f7331761467cf4b1
igorsdt <igorsdt1@gmail.com>
2019-09-28 02:00:16 +0300

    xzbit прокачал твой сайт:)

--- js/common.js
+++ js/common.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
+
 	$('.slider').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		arrows: false,
 		fade: true,
 		asNavFor: '.slider-nav'
-	  });
-	  $('.slider-nav').slick({
+	});
+
+  $('.slider-nav').slick({
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		asNavFor: '.slider',
 		dots: false,
 		centerMode: true,
 		focusOnSelect: true,
-		
-	  });
+  });
+
+  $('.header-navigation__link, .footer-navigation__link').click(function(e){
+  	e.preventDefault()
+  	var attr = $(this).attr('href')
+  	$('html, body').animate({scrollTop:$(attr).position().top}, 1000);
+  })
 })
